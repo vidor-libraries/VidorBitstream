2b0bc62d7b83b79cbcc32e9b289eac0830032d52
Martino Facchin <m.facchin@arduino.cc>
2018-06-15 17:40:19 +0200

    Rethink composite binary creation
    Now the command accepts a comma separated list of filenames with embedded their "nature" and offset
    
    eg:
    
    ./createCompositeBinary -i ../vidor_s_boot.ttf:1:0,../vidor_s.ttf:1:512 -o /tmp/app.ttf
    
    will create a ttf file with the correct bit order for flashing with vidor_s_boot.ttf (type bitstream) at offset 0 and vidor_s.ttf (type bitstream) at offset 512K
    
    ./createCompositeBinary -i ../vidor_s_boot.ttf:1:0,../nios_app.bin:0:512 -o /tmp/boot.ttf -t 1
    
    will create a ttf file with the correct bit order for flashing with vidor_s_boot.ttf (type bitstream) at offset 0 and nios_app.bin (type binary) at offset 512K

--- VidorUtils/extras/createCompositeBinary/make_composite_binary.go
+++ VidorUtils/extras/createCompositeBinary/make_composite_binary.go
@@ -20,88 +20,115 @@ import (
 	"strings"
 )
 
-var offset int = 0x80000
-var fileType int = 1
-var relocateBinaryOffset int = 384 // in kilobytes
+type filesFlag []string
+
+func (h *filesFlag) String() string {
+	return fmt.Sprint(*h)
+}
+
+func (h *filesFlag) Set(csv string) error {
+	var values []string
+	values = strings.Split(csv, ",")
+	for _, value := range values {
+		value = strings.TrimSpace(value)
+		*h = append(*h, value)
+	}
+	return nil
+}
 
 func main() {
 
-	var ttfBitsreamFilename = flag.String("ttf", "../vidor_s.ttf", "filename of ttf produced by Quartus")
-	var invert = flag.Bool("invert", false, "bitwise inverts ttf")
-	var bin = flag.Bool("binary", false, "output a binary file instead than ttf")
-	var niosElfFilename = flag.String("elf", "", "standalone nios elf")
-	var niosBinFilename = flag.String("bin", "../nios_app.bin", "standalone nios bin (already preprocessed with objdump)")
-	var outputFilename = flag.String("out", "../app.", "output filename")
+	var filenames filesFlag
+	compositeOffset := 0
+
+	flag.Var(&filenames, "i", "Comma separated list of files to be merged; format is filename:isBitstream:offset")
+	var fileType = flag.Int("t", 1, "Filetype to generate (boot=0 , app=1, data=2)")
+	var outputFilename = flag.String("o", "../app.ttf", "output filename")
 
 	flag.Parse()
 
-	ttf, err := ioutil.ReadFile(*ttfBitsreamFilename)
-	if err != nil {
-		panic(err)
-	}
-	// file format is %u, , so convert it to this format
-
-	arr := strings.Split(string(ttf), ",")
-
 	var outBuffer []byte
 
-	for _, el := range arr {
-		el = strings.TrimSpace(el)
-		i, err := strconv.Atoi(el)
-		i_b := byte(i)
-		if el != "" && err == nil {
-			if *invert {
-				i_b = reverse(i_b)
+	for _, element := range filenames {
+
+		splits := strings.Split(element, ":")
+		if len(splits) != 3 {
+			panic("Filenames not in the right format!")
+		}
+		filename := splits[0]
+		isBitstream, _ := strconv.Atoi(splits[1])
+		offset, _ := strconv.Atoi(splits[2]) // expressed in kilobytes
+
+		if compositeOffset == -1 {
+			compositeOffset = offset
+		}
+
+		// Pad to offset
+		for len(outBuffer) < (offset * 1024) {
+			outBuffer = append(outBuffer, 0xFF)
+		}
+
+		if isBitstream != 0 {
+
+			ttf, err := ioutil.ReadFile(filename)
+			if err != nil {
+				panic(err)
 			}
-			outBuffer = append(outBuffer, i_b)
-		}
-	}
+			// file format is %u, , so convert it to this format
 
-	// Pad to 384KB
-	for len(outBuffer) < (relocateBinaryOffset * 1024) {
-		outBuffer = append(outBuffer, 0xFF)
-	}
+			arr := strings.Split(string(ttf), ",")
 
-	if *niosElfFilename != "" {
-		// do all the magic with make_app.sh
-	} else {
-		niosBin, err := ioutil.ReadFile(*niosBinFilename)
-		if err != nil {
-			panic(err)
-		}
-		i := 0
-		for i < len(niosBin) {
-			//niosBin[i] = reverse(niosBin[i])
-			i++
-		}
-		outBuffer = append(outBuffer, niosBin...)
-	}
-
-	if *bin {
-		*outputFilename = *outputFilename + "bin"
-		ioutil.WriteFile(*outputFilename, outBuffer, 0666)
-	} else {
-		*outputFilename = *outputFilename + "ttf"
-		outFile, _ := os.Create(*outputFilename)
-		defer outFile.Close()
-		i := 0
-		for i < len(outBuffer) {
-			if len(outBuffer)-i >= 16 {
-				fmt.Fprintf(outFile, "%3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d,", outBuffer[i], outBuffer[i+1], outBuffer[i+2], outBuffer[i+3], outBuffer[i+4], outBuffer[i+5], outBuffer[i+6], outBuffer[i+7], outBuffer[i+8], outBuffer[i+9], outBuffer[i+10], outBuffer[i+11], outBuffer[i+12], outBuffer[i+13], outBuffer[i+14], outBuffer[i+15])
-			} else {
-				// Last line
-				for i < len(outBuffer) {
-					fmt.Fprintf(outFile, "%3d, ", outBuffer[i])
-					i++
+			for _, el := range arr {
+				el = strings.TrimSpace(el)
+				i, err := strconv.Atoi(el)
+				i_b := byte(i)
+				if el != "" && err == nil {
+					i_b = reverse(i_b)
+					outBuffer = append(outBuffer, i_b)
 				}
 			}
-			fmt.Fprintf(outFile, "\n")
-			i += 16
+		} else {
+			bin, err := ioutil.ReadFile(filename)
+			if err != nil {
+				panic(err)
+			}
+			outBuffer = append(outBuffer, bin...)
 		}
-		//ioutil.WriteFile(*outputFilename, []byte(outString), 0666)
 	}
 
+	outFile, _ := os.Create(*outputFilename)
+	defer outFile.Close()
+	i := 0
+	for i < len(outBuffer) {
+		if len(outBuffer)-i >= 16 {
+			fmt.Fprintf(outFile, "%3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d, %3d,", outBuffer[i], outBuffer[i+1], outBuffer[i+2], outBuffer[i+3], outBuffer[i+4], outBuffer[i+5], outBuffer[i+6], outBuffer[i+7], outBuffer[i+8], outBuffer[i+9], outBuffer[i+10], outBuffer[i+11], outBuffer[i+12], outBuffer[i+13], outBuffer[i+14], outBuffer[i+15])
+		} else {
+			// Last line
+			for i < len(outBuffer) {
+				fmt.Fprintf(outFile, "%3d, ", outBuffer[i])
+				i++
+			}
+		}
+		fmt.Fprintf(outFile, "\n")
+		i += 16
+	}
+	//ioutil.WriteFile(*outputFilename, []byte(outString), 0666)
+
+	printInfo(compositeOffset, *fileType, outBuffer, *outputFilename)
+
+}
+
+func printInfo(offset, fileType int, outBuffer []byte, filename string) {
 	// Generate info for application, to paste into __attribute__ ((used, section(".fpga_bitstream_signature"))) section
+
+	if fileType != 0 {
+		fmt.Printf("NO_BOOTLOADER,\n")
+	}
+
+	if fileType == 2 {
+		fmt.Printf("NO_APP,\n")
+	}
+
 	bs := make([]byte, 4)
 	binary.LittleEndian.PutUint32(bs, uint32(offset))
 	fmt.Printf("0x%02x, 0x%02x, 0x%02x, 0x%02x,\n", bs[0], bs[1], bs[2], bs[3])
@@ -109,7 +136,7 @@ func main() {
 	binary.LittleEndian.PutUint32(bs, uint32(len(outBuffer)))
 	fmt.Printf("0x%02x, 0x%02x, 0x%02x, 0x%02x,\n", bs[0], bs[1], bs[2], bs[3])
 
-	f, err := os.Open(*outputFilename)
+	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -130,7 +157,18 @@ func main() {
 	fmt.Println("")
 
 	binary.LittleEndian.PutUint32(bs, uint32(fileType))
-	fmt.Printf("0x%02x, 0x%02x, 0x%02x, 0x%02x,\n", bs[0], bs[1], bs[2], bs[3])
+	fmt.Printf("0x%02x, 0x%02x, 0x%02x, 0x%02x, \n", bs[0], bs[1], bs[2], bs[3])
+
+	fmt.Printf("0x00, 0x00, 0x00, 0x00, // force flag, change to 1 if needed\n")
+
+	if fileType == 0 {
+		fmt.Printf("NO_APP,\n")
+	}
+
+	if fileType != 2 {
+		fmt.Printf("NO_USER_DATA,\n")
+	}
+
 }
 
 func reverse(b uint8) uint8 {
