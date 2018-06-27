538fd59a333f1cdc9804c5991f1b71b6acd6f39f
Riccardo Rizzo <r.rizzo@arduino.cc>
2018-06-27 11:30:48 +0200

    Added Comments
    Added some comments in the example VidorDrawLogo

--- VidorBase/examples/VidorDrawLogo/VidorDrawLogo.ino
+++ VidorBase/examples/VidorDrawLogo/VidorDrawLogo.ino
@@ -16,17 +16,22 @@ void setup() {
   delay(4000);
 }
 
-/**
- * put your main code here, to run repeatedly:
- */
-
 void loop()
 {
   /**
   *  Draw Arduino logo
   */
   vdgfx.fillRect(0,0,640,480,vdgfx.White(),1);
-  vdgfx.GFXText.setCursor(160,440);
+    /**
+  *  The library VIDOR_GFX.h allow to draw some basic elements to the view, like circle rectangular line and point, simply it could be drawed by using the function: 
+  *  - drawPixel()
+  *  - drawLine()
+  *  - drawRect()  
+  *  - fillRect()
+  *  - drawCircle()
+  *  - fillCircle()
+  *  - drawChar()
+  */  
   vdgfx.fillCircle(225,225,100 ,vdgfx.lightBlue(),1);
   vdgfx.fillCircle(415,225,100 ,vdgfx.lightBlue(),1);
   vdgfx.fillCircle(225,225,90 ,vdgfx.White(),1);
@@ -34,6 +39,11 @@ void loop()
   vdgfx.fillRect(175,220,100,10 ,vdgfx.lightBlue(),1);
   vdgfx.fillRect(365,220,100,10 ,vdgfx.lightBlue(),1);
   vdgfx.fillRect(410,175,10,100 ,vdgfx.lightBlue(),1);
+  /**
+  *  for draw a text in the view could be used the function write() or print(),
+  *  the text feature like color, size, position and alpha could be setted by the 
+  *  functions textAlpha(), setCursor(), textColor() and textSize().
+  */
   vdgfx.GFXText.setCursor(150,375);
   vdgfx.GFXText.textAlpha(1);
   vdgfx.GFXText.textSize(3);
