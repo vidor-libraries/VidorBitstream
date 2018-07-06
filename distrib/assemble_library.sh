ecdf9814b9b523f6eadb24e25827d6c336cd2eb2
Martino Facchin <m.facchin@arduino.cc>
2018-07-06 11:57:16 +0200

    Add libs to deploy script

--- distrib/assemble_library.sh
+++ distrib/assemble_library.sh
@@ -8,24 +8,35 @@ cd $BASE_FOLDER/../
 declare -a libs=("FPGA_GFX_COUNT" "FPGA_SPI_INTERFACES_COUNT" "FPGA_I2C_INTERFACES_COUNT" "FPGA_UART_INTERFACES_COUNT" "FPGA_ENCODERS_COUNT" "FPGA_NEOPIXEL_COUNT" "FPGA_CAMERA_COUNT" )
 declare -a folders=("Vidor_GFX" "VidorSPI" "VidorI2C" "VidorUART" "VidorEncoder" "VidorNeopixel" "VidorCamera")
 
-
 mkdir -p ./distrib/$LIB_NAME/
 
 cp -r $LIB_NAME/* ./distrib/$LIB_NAME/
 
-# get length of an array
 arraylength=${#libs[@]}
 
 for (( i=1; i<${arraylength}; i++ ));
 do
 HAS_LIB=`cat $LIB_NAME/src/defines.h | grep ${libs[i]}  | expand | tr -s ' ' | cut -f3 -d' ' `
 
-echo ${folders[i]} $HAS_LIB
-
 if [ x$HAS_LIB != x ] && [ $HAS_LIB != 0 ]
 then
 	cp ${folders[i]}/* ./distrib/$LIB_NAME/src/
+	cp ${folders[i]}/examples ./distrib/$LIB_NAME/examples/
 fi
 
 done
 
+# default libraries (VidorCommon/VidorIO)
+declare -a defaultlibs=("VidorIO" "VidorCommon")
+arraylength=${#defaultlibs[@]}
+
+for (( i=1; i<${arraylength}; i++ ));
+do
+cp  ${defaultlibs[i]}/* ./distrib/$LIB_NAME/src/
+cp -r ${defaultlibs[i]}/examples/ ./distrib/$LIB_NAME/examples/
+
+done
+
+# copy VidorUtils (not sure about this one)
+cp -r VidorUtils/src/* ./distrib/$LIB_NAME/src/
+#don't copy examples
