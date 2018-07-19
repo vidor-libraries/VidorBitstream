8e94921bffcf46d4a3829466b959d9b4e37c6803
Riccardo Rizzo <37290960+Rocketct@users.noreply.github.com>
2018-07-19 12:02:09 +0200

    Modified VidorQrRecognition Example (#6)

--- VidorCamera/examples/VidorQrRecognition/VidorQrRecognition.ino
+++ VidorCamera/examples/VidorQrRecognition/VidorQrRecognition.ino
@@ -3,7 +3,18 @@
 
 VidorCamera vcam;
 
+#define MAXDIM 10
+
 static uint16_t x[QR_PT_DET_NUM], y[QR_PT_DET_NUM];
+
+struct qrPtn {
+  uint16_t x[QR_PT_DET_NUM];
+  uint16_t y[QR_PT_DET_NUM];
+};
+
+static qrPtn qrBufferPtn[MAXDIM];
+
+uint16_t count = 0, last;
 
 void setup() {
   Serial.begin(115200);
@@ -37,9 +48,15 @@ void loop()  {
       x[i] = (vcam.qrrec.qr.pt[i].xs + vcam.qrrec.qr.pt[i].xe) / 2;
       y[i] = (vcam.qrrec.qr.pt[i].ys + vcam.qrrec.qr.pt[i].ye) / 2;
       vcam.vgfx.Cross(x[i], y[i], 65535);
+
     }
   }
+
+  last = count % MAXDIM;
   for (int i = 0; i < QR_PT_DET_NUM; i++) {
-    vcam.vgfx.Cross(x[i], y[i], 0, 0);
+    vcam.vgfx.Cross(qrBufferPtn[last].x[i], qrBufferPtn[last].y[i], 0, 0);
+    qrBufferPtn[last].x[i] = x[i];
+    qrBufferPtn[last].y[i] = y[i];
   }
+  count++;
 }
