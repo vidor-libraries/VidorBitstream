9c46b652ed84c4c98d04e60294e1cba8d0b9257e
Martino Facchin <m.facchin@arduino.cc>
2018-07-19 12:03:39 +0200

    Improve VidorBase example

--- VidorBase/examples/VidorTestSketch/VidorTestSketch.ino
+++ VidorBase/examples/VidorTestSketch/VidorTestSketch.ino
@@ -1,35 +1,70 @@
+/*
+   This sketch shows how to interact with the "standard" Vidor bitstream.
+   A bitstream is like a program, but rather than containing code it describes how the FPGA should behave.
+   Every Vidor bitstream (and library) provides a common set of functionalities
+   Moreover, the library also includes all the "building block" as standard Arduino libraries
+*/
+
 #include "VidorBase.h"
-#include "VidorI2C.h"
-#include "VidorSPI.h"
-#include "VidorUART.h"
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
+
+  // The FPGA bitstream is automatically loaded at board startup.
+  // However, we can stop the FPGA and reload its functionality at any time
+
+  // Let's start by stopping the FPGA
+  FPGA.end();
+
+  // Now the FPGA is off, let's restart it
   if (!FPGA.begin()) {
     Serial.println("Initialization failed!");
     while (1) {}
   }
-  Serial.println(FPGA.read(0), HEX);
-  Serial.println(FPGA.read(1), HEX);
-  Serial.println(FPGA.read(2), HEX);
-  Serial.println(FPGA.read(3), HEX);
-  Serial.println(FPGA.read(4), HEX);
 
-  pinMode(105, OUTPUT);
-  digitalWrite(105, HIGH);
+  // We are back to the normal functionalities, so let's discover which version we are running
+  int version = FPGA.version();
+  Serial.print("Vidor bitstream version: ");
+  Serial.println(version, HEX);
 
-  WireEx.begin();
+  // Let's also ask which IPs are included in this bitstream
+  FPGA.printConfig();
 
-  SPIEx.begin();
-  SPIEx.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
-  SPIEx.transfer(12);
-  SPIEx.endTransaction();
+  // Ok, so we know now that the FPGA contains the extended GPIO IP
+  // The GPIO pins controlled by the FPGA start from 100
+  // Please refer to the online documentation for the actual pin assignment
+  // Let's configure pin A0 to be an output, controlled by the FPGA
+  pinMode(133, OUTPUT);
+  digitalWrite(133, HIGH);
 
-  SerialEx.begin(115200);
-  while (!SerialEx);
-  SerialEx.println("test");
+  // The same pin can be read by the SAMD processor :)
+  pinMode(A0, INPUT);
+  Serial.print("Pin A0 is ");
+  Serial.println(digitalRead(A0) == LOW ? "LOW" : "HIGH");
 
+  digitalWrite(133, LOW);
+  Serial.print("Pin A0 is ");
+  Serial.println(digitalRead(A0) == LOW ? "LOW" : "HIGH");
+
+  // The VidorBase bitstream provides various extra peripherals, that can be controlled with the standard Arduino API
+  // To find which pins you need to use, please refer to the onlune documentation
+
+  // WireFPGA1 object provides an extra I2C interface
+  WireFPGA1.begin();
+
+  // Same for SPIFPGA1, but with SPI of course
+  SPIFPGA1.begin();
+  SPIFPGA1.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
+  SPIFPGA1.transfer(12);
+  SPIFPGA1.endTransaction();
+
+  // Same for Serial interfaces :)
+  SerialFPGA1.begin(115200);
+  while (!SerialFPGA1);
+  SerialFPGA1.println("test");
+
+  // Ok, we are done, let's shutdown the FPGA (to save power, for example)
   FPGA.end();
 }
 
