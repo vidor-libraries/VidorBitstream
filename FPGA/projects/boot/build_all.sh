feb9a2ec2cf50e36dbe4472d385b1a5f64e61c92
Dario Pennisi <dario@iptronix.com>
2018-08-09 22:08:01 +0200

    NIOS - included makecompositebinary in build_All scripts
    Change-Id: I72de9796324f99f4bf6b2f6e5266e1688a5b73f3

--- FPGA/projects/boot/build_all.sh
+++ FPGA/projects/boot/build_all.sh
@@ -2,3 +2,5 @@ PROJECT_NAME=${PWD##*/}
 qsys-generate --synthesis=VERILOG $PROJECT_NAME.qsys
 source build_sw.sh
 quartus_sh --flow compile $PROJECT_NAME
+
+../../../TOOLS/makeCompositeBinary/MakeCompositeBinary -i output_files/$PROJECT_NAME.ttf:1:0 -o output_files/app.ttf -t 0
