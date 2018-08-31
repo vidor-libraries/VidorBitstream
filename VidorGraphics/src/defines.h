de0fa7e3e22ad460e20fbe8cfbaeb158df2e3860
Martino Facchin <m.facchin@arduino.cc>
2018-08-31 16:10:46 +0200

    Add NiNa defines to Graphics lib

--- VidorGraphics/src/defines.h
+++ VidorGraphics/src/defines.h
@@ -1,3 +1,22 @@
+/*
+  This file is part of the VidorGraphics library.
+  Copyright (c) 2018 Arduino SA. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include "common.h"
 
 #ifndef __DEFINES_VIDOR_H__
@@ -33,6 +52,9 @@
 
 // NINA signals
 #define SPIWIFI              SPIEx
+#define SerialNina           SerialEx
+#define FPGA_NINA_TX         (64 + 16)
+#define FPGA_NINA_RX         (64 + 15)
 #define FPGA_NINA_MOSI       (64 + 19)
 #define FPGA_NINA_MISO       (64 + 20)
 #define FPGA_NINA_SCK        (64 + 18)
