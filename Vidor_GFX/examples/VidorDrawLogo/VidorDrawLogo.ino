5f8d3f4da035fd289fa840a10068c52e17050eae
Sandeep Mistry <s.mistry@arduino.cc>
2018-09-04 15:41:04 -0400

    Correct alpha being 8-bits now

--- Vidor_GFX/examples/VidorDrawLogo/VidorDrawLogo.ino
+++ Vidor_GFX/examples/VidorDrawLogo/VidorDrawLogo.ino
@@ -26,26 +26,26 @@ void loop()
   */
 
   // Fill the screen with a white background
-  vdgfx.fillRect(0,0,640,480,vdgfx.White(),1);
+  vdgfx.fillRect(0,0,640,480,vdgfx.White());
 
   /**
   *  The library allows drawing some basic elements to the view, like circles, rectangles, lines
   */  
 
-  vdgfx.fillCircle(225,225,100 ,vdgfx.lightBlue(),1);
-  vdgfx.fillCircle(415,225,100 ,vdgfx.lightBlue(),1);
-  vdgfx.fillCircle(225,225,90 ,vdgfx.White(),1);
-  vdgfx.fillCircle(415,225,90 ,vdgfx.White(),1);
-  vdgfx.fillRect(175,220,100,10 ,vdgfx.lightBlue(),1);
-  vdgfx.fillRect(365,220,100,10 ,vdgfx.lightBlue(),1);
-  vdgfx.fillRect(410,175,10,100 ,vdgfx.lightBlue(),1);
+  vdgfx.fillCircle(225,225,100 ,vdgfx.lightBlue());
+  vdgfx.fillCircle(415,225,100 ,vdgfx.lightBlue());
+  vdgfx.fillCircle(225,225,90 ,vdgfx.White());
+  vdgfx.fillCircle(415,225,90 ,vdgfx.White());
+  vdgfx.fillRect(175,220,100,10 ,vdgfx.lightBlue());
+  vdgfx.fillRect(365,220,100,10 ,vdgfx.lightBlue());
+  vdgfx.fillRect(410,175,10,100 ,vdgfx.lightBlue());
 
   /**
   *  To draw a text we can use the classic functions like write() and print()
   *  Text size, color and position can be changed using the .text subclass
   */
   vdgfx.text.setCursor(150,375);
-  vdgfx.text.setAlpha(1);
+  vdgfx.text.setAlpha(255);
   vdgfx.text.setSize(3);
   vdgfx.text.setColor(vdgfx.lightBlue());
   vdgfx.println("ARDUINO");
