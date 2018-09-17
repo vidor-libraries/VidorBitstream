d3c761cd3e33325cfedf119183d730cc975383ec
Martino Facchin <m.facchin@arduino.cc>
2018-09-17 16:48:44 +0200

    Fix assemble_library script for new repo structure

--- TOOLS/scripts/assemble_library.sh
+++ TOOLS/scripts/assemble_library.sh
@@ -3,7 +3,16 @@
 LIB_NAME=$1
 BASE_FOLDER=`dirname $0`
 
-cd $BASE_FOLDER/../
+for (( i=0; ; i++ ));
+do
+    ls | grep ip && ls | grep projects
+    res=$?
+    if [ $res != 0 ]; then
+        cd ..
+    else
+        break
+    fi
+done
 
 declare -a libs=("FPGA_GFX_COUNT" "FPGA_SPI_INTERFACES_COUNT" "FPGA_I2C_INTERFACES_COUNT" "FPGA_UART_INTERFACES_COUNT" "FPGA_ENCODERS_COUNT" "FPGA_NEOPIXEL_COUNT" "FPGA_CAMERA_COUNT" )
 declare -a folders=("Vidor_GFX" "VidorSPI" "VidorI2C" "VidorUART" "VidorEncoder" "VidorNeopixel" "VidorCamera")
@@ -11,13 +20,21 @@ declare -a folders=("Vidor_GFX" "VidorSPI" "VidorI2C" "VidorUART" "VidorEncoder"
 mkdir -p ./distrib/$LIB_NAME/
 rm -rf ./distrib/$LIB_NAME/*
 
-cp -a $LIB_NAME/* ./distrib/$LIB_NAME/.
+#TODO: temporary, the library will be created at runtime
+cp -a projects/precompiled/$LIB_NAME/* ./distrib/$LIB_NAME/.
 
 arraylength=${#libs[@]}
 
 for (( i=0; i<${arraylength}; i++ ));
 do
-HAS_LIB=`cat $LIB_NAME/src/defines.h | grep ${libs[i]}  | expand | tr -s ' ' | cut -f3 -d' ' `
+#update folders with real path
+folderName=${folders[i]}
+folders[i]=`find . | grep $folderName  | grep ip | head -n1`
+done
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+HAS_LIB=`cat projects/precompiled/$LIB_NAME/src/defines.h | grep ${libs[i]}  | expand | tr -s ' ' | cut -f3 -d' ' `
 
 if [ x$HAS_LIB != x ] && [ $HAS_LIB != 0 ]
 then
@@ -35,17 +52,20 @@ fi
 done
 
 # default libraries (VidorCommon/VidorIO)
-declare -a defaultlibs=("VidorIO" "VidorCommon")
+declare -a defaultlibs=("VidorIO" "VidorCommon" "VidorUtils/src")
 arraylength=${#defaultlibs[@]}
 
 for (( i=0; i<${arraylength}; i++ ));
 do
-	cp ${defaultlibs[i]}/* ./distrib/$LIB_NAME/src/.
+#update folders with real path
+folderName=${defaultlibs[i]}
+defaultlibs[i]=`find . | grep $folderName  | grep ip | head -n1`
+done
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+	cp -a ${defaultlibs[i]}/* ./distrib/$LIB_NAME/src/.
 	if [ -d ${defaultlibs[i]}/examples/ ]; then
 		cp -a ${defaultlibs[i]}/examples/* ./distrib/$LIB_NAME/examples/.
 	fi
 done
-
-# copy VidorUtils (not sure about this one)
-cp -a VidorUtils/src/* ./distrib/$LIB_NAME/src/.
-#don't copy examples
