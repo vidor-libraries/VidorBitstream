f05a896dfc0d9d5599f249dea9548cc9d53521e8
Martino Facchin <m.facchin@arduino.cc>
2018-10-24 15:28:06 +0200

    Revert "Remove UART drivers from lite package"
    This reverts commit fd68429caa85fdef09122dcdcb96202d4e90e4d1.

--- TOOLS/scripts/build_sw.sh
+++ TOOLS/scripts/build_sw.sh
@@ -57,6 +57,7 @@ BSP_FLAGS=" \
 --cmd enable_sw_package MAILBOX \
 --cmd enable_sw_package RPC  \
 --cmd enable_sw_package GFX  \
+--cmd enable_sw_package UART  \
 --set hal.enable_c_plus_plus 0 \
 --set hal.enable_clean_exit 0 \
 --set hal.enable_exit 0 \
