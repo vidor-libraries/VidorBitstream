101c5876f625a3be6b4ea0dccc9d2d6cb6113047
MaxAgneni <m.agneni@framas.it>
2018-11-08 14:22:55 +0100

    Fix compilation for RAM + FLASH

--- TOOLS/scripts/create_image.sh
+++ TOOLS/scripts/create_image.sh
@@ -1,8 +1,10 @@
 PROJECT_NAME=${PWD##*/}
-if [ -f software/softcore ]; then
+if [ -d software/softcore ]; then
+echo create ram + flash app.ttf
 elf2flash --input=build/software/$PROJECT_NAME/$PROJECT_NAME.elf --output=build/output_files/$PROJECT_NAME.flash --base=0x008E0000 --end=0x008FFFFF --verbose --save
 nios2-elf-objcopy --input-target srec --output-target binary build/output_files/$PROJECT_NAME.flash build/output_files/$PROJECT_NAME.bin
-../../TOOLS/makeCompositeBinary/makeCompositeBinary -i build/output_files/$PROJECT_NAME.ttf:1:512,build/output_files/$PROJECT_NAME.bin:0:896 -o build/output_files/app.ttf -t 1 >output_files/signature.h
+../../TOOLS/makeCompositeBinary/makeCompositeBinary -i build/output_files/$PROJECT_NAME.ttf:1:512,build/output_files/$PROJECT_NAME.bin:0:896 -o build/output_files/app.ttf -t 1 > build/output_files/signature.h
 else
-../../TOOLS/makeCompositeBinary/makeCompositeBinary -i build/output_files/$PROJECT_NAME.ttf:1:512 -o build/output_files/app.ttf -t 1 >build/output_files/signature.h
+echo create ram app.ttf
+../../TOOLS/makeCompositeBinary/makeCompositeBinary -i build/output_files/$PROJECT_NAME.ttf:1:512 -o build/output_files/app.ttf -t 1 > build/output_files/signature.h
 fi
