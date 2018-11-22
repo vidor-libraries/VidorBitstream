bbf6f591a9832a1b1176596fa1e140ce5a276e09
Martino Facchin <m.facchin@arduino.cc>
2018-11-22 12:38:14 +0100

    Fix naming to VidorPeripherals

--- projects/MKRVIDOR4000_peripherals/software/arduino/src/defines.h
+++ projects/MKRVIDOR4000_peripherals/software/arduino/src/defines.h
@@ -1,5 +1,5 @@
 /*
-  This file is part of the VidorGraphics library.
+  This file is part of the VidorPeripherals library.
   Copyright (c) 2018 Arduino SA. All rights reserved.
 
   This library is free software; you can redistribute it and/or
@@ -22,30 +22,24 @@
 #ifndef __DEFINES_VIDOR_H__
 #define __DEFINES_VIDOR_H__
 
-#define FPGA_SPI_INTERFACES_COUNT 1
-#define FPGA_I2C_INTERFACES_COUNT 2
-#define FPGA_UART_INTERFACES_COUNT  1
-#define FPGA_ENCODERS_COUNT     11
-#define FPGA_NEOPIXEL_COUNT     11
-#define FPGA_CAMERA_COUNT     1
-#define FPGA_GFX_COUNT        1
-#define FPGA_QR_COUNT       1
+#define FPGA_SPI_INTERFACES_COUNT	6
+#define FPGA_I2C_INTERFACES_COUNT	4
+#define FPGA_UART_INTERFACES_COUNT	9
+#define FPGA_ENCODERS_COUNT			11
+#define FPGA_NEOPIXEL_COUNT			4
+#define FPGA_CAMERA_COUNT			0
+#define FPGA_GFX_COUNT				0
+#define FPGA_QR_COUNT				0
 
-#define GPIO_NUM_OFFSET       100
+#define GPIO_NUM_OFFSET				100
 
 #define IRQ_PIN           33
 
-#define NEOPIXEL_PIN_0        A0
-#define NEOPIXEL_PIN_1        A1
-#define NEOPIXEL_PIN_2        A2
-#define NEOPIXEL_PIN_3        A3
-#define NEOPIXEL_PIN_4        A4
-#define NEOPIXEL_PIN_5        A5
-#define NEOPIXEL_PIN_6        A6
-#define NEOPIXEL_PIN_7        0
-#define NEOPIXEL_PIN_8        1
-#define NEOPIXEL_PIN_9        2
-#define NEOPIXEL_PIN_10       3
+#define NEOPIXEL_PIN_0				A6
+#define NEOPIXEL_PIN_1				0
+#define NEOPIXEL_PIN_2				7
+#define NEOPIXEL_PIN_3				8
+#define NEOPIXEL_PIN_4				12
 
 #define NEOPIXEL_PINMUX       4
 #define NEOPIXEL_FB_INDEX     1
