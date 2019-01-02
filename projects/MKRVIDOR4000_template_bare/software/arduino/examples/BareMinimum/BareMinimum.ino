e098c778ef6aaaec204a66dfb3e1bebdc395ca13
Martino Facchin <m.facchin@arduino.cc>
2019-01-02 17:26:54 +0100

    [template_bare] Add BareMinimum example and library

---
+++ projects/MKRVIDOR4000_template_bare/software/arduino/examples/BareMinimum/BareMinimum.ino
@@ -0,0 +1,11 @@
+#include "VidorTemplateBare.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  FPGA.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
